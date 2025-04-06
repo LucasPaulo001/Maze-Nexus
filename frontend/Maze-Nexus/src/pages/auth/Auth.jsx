fe0e2d760723726561fe34a2287edc456b2885cc
Lucas Paulo <lp152659@gmail.com>
2025-04-06 14:58:35 -0300

    Resolução de incompatibilidades de importações

--- frontend/Maze-Nexus/src/pages/auth/Auth.jsx
+++ frontend/Maze-Nexus/src/pages/auth/Auth.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import styles from './Auth.module.css'
 import '../../App.css'
 import { Link } from "react-router-dom"
-import Otpinput from "./otpInput/OTPinput"
+import Otpinput from "./otpInput/Otpinput"
 import { useNavigate } from "react-router-dom"
 const urlVerify = 'http://localhost:1526/user/verifyCode'
 
